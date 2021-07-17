e608d07f186085fe14966298533a9c878a6b7b34
josethz00 <jose.soares18@fatec.sp.gov.br>
2021-07-17 16:46:09 -0300

    feat: .h file with reserved words extern constants

---
+++ src/constants/keywords-constants.h
@@ -0,0 +1,11 @@
+#ifndef KEYWORDS_CONSTANTS
+#define KEYWORDS_CONSTANTS
+
+extern const char * KW_NUMBER;
+extern const char * KW_BOOLEAN;
+extern const char * KW_STRING;
+extern const char * KW_UNDEFINED;
+
+int isKeyword(char * value);
+
+#endif
