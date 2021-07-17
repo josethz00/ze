fc89f23be0e6e36e2f87f771da35d43a3d1521f0
josethz00 <jose.soares18@fatec.sp.gov.br>
2021-07-17 16:45:51 -0300

    feat: .c file with quapa language reserved words

---
+++ src/constants/keywords-constants.c
@@ -0,0 +1,21 @@
+#include "./keywords-constants.h"
+#include <stdio.h>
+#include <string.h>
+
+const char * KW_NUMBER = "number";
+const char * KW_BOOLEAN = "boolean";
+const char * KW_STRING = "string";
+const char * KW_UNDEFINED = "undefined";
+
+int isKeyword(char * value) {
+
+  while (
+    strcmp(value, KW_NUMBER) ||
+    strcmp(value, KW_BOOLEAN) ||
+    strcmp(value, KW_STRING) ||
+    strcmp(value, KW_UNDEFINED)
+  ) {
+    return 1;
+  }
+  return 0;
+}
